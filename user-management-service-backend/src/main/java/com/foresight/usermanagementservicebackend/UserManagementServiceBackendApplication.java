aa91c1a88c53c63ddaac5ac995281c367c72ffed
Abdelrahman Essam <abdelrahman.essaam@gmail.com>
2023-12-29 13:08:16 +0200

    hh

---
+++ user-management-service-backend/src/main/java/com/foresight/usermanagementservicebackend/UserManagementServiceBackendApplication.java
@@ -0,0 +1,50 @@
+package com.foresight.usermanagementservicebackend;
+
+
+
+
+import com.foresight.usermanagementservicebackend.entity.UserRole;
+import com.foresight.usermanagementservicebackend.model.UserCreateRequest;
+import com.foresight.usermanagementservicebackend.service.UserService;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableRabbit
+public class UserManagementServiceBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementServiceBackendApplication.class, args);
+    }
+    @Bean
+    @Autowired
+    CommandLineRunner runner(UserService service)
+    {
+        return (x)->
+        {
+            try{
+                service.addUser(new UserCreateRequest("Abdelrahman","essam","abdo@foresight.com","123456aA@", UserRole.ADMIN,true));
+
+
+            }
+            catch (RuntimeException ex)
+            {
+
+            }
+        };
+
+    }
+
+
+
+}
+
+//TODO make userSummary dto to transfer list of users
+//TODO handle password forgetting scenario
+// TODO user search and filtering functionality
+// TODO email should end with @foresight.com
+// TODO pagination
+// TODO role based functionality
---
+++ user-management-service-backend/src/main/java/com/foresight/usermanagementservicebackend/model/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.foresight.usermanagementservicebackend.model;
+
+import com.foresight.usermanagementservicebackend.entity.UserRole;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+    private Long id;
+    private UserRole role;
+}
